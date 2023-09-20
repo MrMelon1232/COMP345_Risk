4202050daf02bad643117534cd324a0ba4a1f3b0
Bayazeed Rahman <99050468+blazefire16@users.noreply.github.com>
2023-09-19 23:24:21 -0400

    Created Orders file, created virtual functions

---
+++ Orders.cpp
@@ -0,0 +1,38 @@
+#include "Orders.h"
+#include <Vector>
+#include <iostream>
+
+
+class Order
+{
+    public:
+
+
+
+
+};
+
+
+class OrderList
+{
+    private: 
+        std::vector<Order*> ordersList;
+
+    public:
+    //Default Constructor
+        OrderList()
+        {
+      
+        }      
+
+        OrderList(OrderList &O)
+        {
+                
+        }
+
+};
+
+
+    
+         
+    
---
+++ Orders.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+class Order 
+{
+    public: 
+        virtual ~Order() = default; 
+        virtual void execute() = 0; 
+        virtual bool validate() = 0; 
+        virtual std::string getDescription() const = 0;
+        virtual Order* copy() const = 0;
+}; 
+
+class Deploy : public Order
+{
+    public:
+        Deploy();
+        virtual ~Deploy() = default; 
+        Deploy(const Deploy& d);
+        void execute() override;
+        bool validate() const override;
+
+
+};
+
