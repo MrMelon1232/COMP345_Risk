05354715e9b45fb5c3bfd69a76b39f794dbd771d
shimdevkun <shimdevkun@gmail.com>
2023-09-25 14:27:43 -0400

    Implement GameEngine for assignment 1

---
+++ includes/GameEngineDriver.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "GameEngine.h"
+
+void testGameStates();
---
+++ src/GameEngine.cpp
@@ -0,0 +1,137 @@
+#include "GameEngine.h"
+#include <algorithm>
+
+// Constructor to initialize a state with a name.
+State::State(string name) { 
+    this->name = name; 
+};
+
+// Copy constructor for State class.
+State::State(State& state) {
+    name = state.name;
+    transitions = state.transitions;
+}
+
+// Getter for the State's name.
+string State::getName() {
+    return name;
+}
+
+// Getter for the State's transitions.
+vector<Transition*> State::getTransitions() {
+    return transitions;
+}
+
+// Function that can add multiple transitions for this state.
+void State::addTransitions(vector<Transition*> transitions...) {
+    this->transitions = transitions;
+}
+
+// Assignment operator overload for State class.
+State& State::operator=(const State& state) {
+    name = state.name;
+    transitions = state.transitions;
+    return *this;
+}
+
+// Output stream operator overload to return the state's name.
+std::ostream& operator<<(std::ostream& output, const State& state) {
+    output << "`" << state.name << "`";
+    return output;
+}
+
+// Constructor to initialize transition with command name and state to transition to.
+Transition::Transition(string commandName, State* nextState) {
+    this->commandName = commandName;
+    this->nextState = nextState;
+};
+
+// Copy constructor for Transition class.
+Transition::Transition(Transition& transition) {
+    commandName = transition.commandName;
+    nextState = transition.nextState;
+}
+
+// Getter for Transition's command name.
+string Transition::getCommandName() {
+    return commandName;
+}
+
+// Getter for Transition's next state.
+State* Transition::getNextState() {
+    return nextState;
+}
+
+// Assignment operator overload for Transition class.
+Transition& Transition::operator=(const Transition& transition) {
+    commandName = transition.commandName;
+    nextState = transition.nextState;
+    return *this;
+}
+
+// Ouput stream operator overload to return Transition's command name and next state.
+std::ostream& operator<<(std::ostream& output, const Transition& transition) {
+    output << "Command name: " << transition.commandName << 
+            ", Next state: " << transition.nextState->getName();
+    return output;
+}
+
+// Variable that holds the current state of the game.
+State* currentState;
+
+// Function that creates the state and transition objects for the game engine. It also sets the initial state.
+void initStateAndTransitions() {
+    State* start = new State("start");
+    State* mapLoaded = new State("maploaded");
+    State* mapValidated = new State("mapvalidated");
+    State* playersAdded = new State("playersadded");
+    State* assignReinforcements = new State("assignreinforcement");
+    State* issueOrders = new State("issueorders");
+    State* executeOrders = new State("executeorders");
+    State* win = new State("win");
+
+    Transition* loadMap = new Transition("loadmap", mapLoaded);
+    Transition* validateMap = new Transition("validatemap", mapValidated);
+    Transition* addPlayer = new Transition("addplayer", playersAdded);
+    Transition* assignCountries = new Transition("assigncountries", assignReinforcements);
+    Transition* issueOrder = new Transition("issueorder", issueOrders);
+    Transition* endIssueOrders = new Transition("endissueorders", executeOrders);
+    Transition* execOrder = new Transition("execorder", executeOrders);
+    Transition* endExecOrders = new Transition("endexecorders", assignReinforcements);
+    Transition* winTrans = new Transition("win", win);
+    Transition* play = new Transition("play", start);
+    Transition* end = new Transition("end", nullptr);
+
+    start->addTransitions({loadMap});
+    mapLoaded->addTransitions({loadMap, validateMap});
+    mapValidated->addTransitions({addPlayer});
+    playersAdded->addTransitions({addPlayer, assignCountries});
+    assignReinforcements->addTransitions({issueOrder});
+    issueOrders->addTransitions({issueOrder, endIssueOrders});
+    executeOrders->addTransitions({execOrder, winTrans, endExecOrders});
+    win->addTransitions({play, end});
+
+    currentState = start;
+    std::cout << "Current state is " << *currentState << "." << std::endl;
+}
+
+// Function that finds transition with given command name. If not found, it displays an error. Otherwise, calls function to perform the state transition.
+void findAndTransition(string name) {
+    vector<Transition*> transitions = currentState->getTransitions();
+    vector<Transition*>::iterator it = std::find_if(transitions.begin(), transitions.end(), 
+                                         [&name](Transition* cmd) { return cmd->getCommandName() == name; });
+    if (it != transitions.end())
+        transition(*it);
+    else
+        std::cout << "Invalid command for state " << *currentState << "." << std::endl;
+}
+
+// Function that sets the current game engine's state to the transition's next state.
+void transition(Transition* transition) {
+    currentState = transition->getNextState();
+    std::cout << "Command `" << transition->getCommandName() << "` has been executed. ";
+    if (currentState)
+        std::cout << "Current state is now " << *currentState  << "." << std::endl;
+    else
+        std::cout << "The game cycle has been completed." << std::endl; 
+}
