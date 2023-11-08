7509036ac61925cb4266fe1c32c093156e7b7933
NavidFaiyaz <99299538+NavidFaiyaz@users.noreply.github.com>
2023-11-08 16:42:29 -0500

    Update GameEngine.h

--- includes/GameEngine.h
+++ includes/GameEngine.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Map.h"
 #include "CommandProcessing.h"
+#include "Player.h"
 #include <iostream>
 #include <vector>
 
@@ -11,69 +12,69 @@ class Transition;
 
 // Represents a state of the game with its valid commands
 class State {
-    public:
-        State(string name);
-        State(State& state);
-        string getName();
-        vector<Transition*> getTransitions();
-        void addTransitions(vector<Transition*> transitions...);
-        State& operator=(const State& state);
-        friend std::ostream& operator<<(std::ostream& output, const State& state);
-        ~State();
-    private:
-        string name;
-        vector<Transition*> transitions;
+public:
+    State(string name);
+    State(State& state);
+    string getName();
+    vector<Transition*> getTransitions();
+    void addTransitions(vector<Transition*> transitions...);
+    State& operator=(const State& state);
+    friend std::ostream& operator<<(std::ostream& output, const State& state);
+    ~State();
+    
+private:
+    string name;
+    vector<Transition*> transitions;
 };
 
 // Holds the next state to transition to after executing the command with the given name
 class Transition {
-    public:
-        Transition(string commandName, State* nextState);
-        Transition(Transition& transition);
-        string getCommandName();
-        State* getNextState();
-        Transition& operator=(const Transition& transition);
-        friend std::ostream& operator<<(std::ostream& output, const Transition& transition);
-        ~Transition();
-    private:
-        string commandName;
-        State* nextState;
+public:
+    Transition(string commandName, State* nextState);
+    Transition(Transition& transition);
+    string getCommandName();
+    State* getNextState();
+    Transition& operator=(const Transition& transition);
+    friend std::ostream& operator<<(std::ostream& output, const Transition& transition);
+    ~Transition();
+private:
+    string commandName;
+    State* nextState;
 };
 
 class CommandProcessor; // forward declaration
 
 class GameEngine {
-    public:
-        GameEngine();
-        GameEngine(string mode);
-        GameEngine(vector<State*> states);
-        GameEngine(GameEngine& gameEngine);
+public:
+    GameEngine();
+    GameEngine(vector<State*> states);
+    GameEngine(GameEngine& gameEngine);
+    vector<Player*> players;
 
-        // Getters and setters.
-        string getMode() { return mode; }
-        void setMode(string mode) { this->mode = mode; }
-        State* getCurrentState() { return currentState; }
-        Map* getCurrentMap() { return currentMap; }
-        void setCurrentMap(Map* map) { this->currentMap = map; }
-        CommandProcessor* getCommandProcessor() { return commandProcessor; }
-        void setCommandProcessor(CommandProcessor* commandProcessor) {this->commandProcessor = commandProcessor; }
+    MapLoader* mapLoader;
 
-        bool isCommandValid(string command);
-        void findAndTransition(string name);
-        void transition(Transition *transition);
-        
-        GameEngine& operator=(const GameEngine& gameEngine);
-        friend std::ostream& operator<<(std::ostream& output, const GameEngine& gameEngine);
 
-        ~GameEngine();
-    private:
-        void initProcessor();
-        void setDefaultGameStates();
-        void selectMode();
+    vector<State*> getStates() { return states; };
+    State* getCurrentState() { return currentState; };
+    Map* getCurrentMap() { return currentMap; };
+    void setCurrentMap(Map* map) { this->currentMap = map; };
+    void setCommandProcessor(CommandProcessor* commandProcessor) { this->commandProcessor = commandProcessor; };
+    void addPlayer(Player* player);
 
-        vector<State*> states;
-        State* currentState;
-        Map* currentMap;
-        CommandProcessor* commandProcessor;
-        string mode;
+    bool isCommandValid(string command);
+    void findAndTransition(string name);
+    void transition(Transition* transition);
+
+    GameEngine& operator=(const GameEngine& gameEngine);
+    friend std::ostream& operator<<(std::ostream& output, const GameEngine& gameEngine);
+
+    ~GameEngine();
+    void startupPhase();
+    void distributeTerritories(int num);
+private:
+    vector<State*> states;
+    State* currentState;
+    Map* currentMap;
+    CommandProcessor* commandProcessor;
+    vector<Player*> players;
 };
