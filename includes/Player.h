283f8c0d9c813b4c2015d922efc123a92a4b51ee
NavidFaiyaz <99299538+NavidFaiyaz@users.noreply.github.com>
2023-11-09 19:25:20 -0500

    Update Player.h

--- includes/Player.h
+++ includes/Player.h
@@ -12,63 +12,81 @@ using namespace std;
 class Territory;
 class OrdersList;
 class Hand;
+class Card;
 enum class OrderType;
 
 // Declaration of Player class
-class Player 
+class Player
 {
     // Players' possessions
-    private:
+private:
 
-        // List of territories owned by the player
-        std::vector<Territory*> territoriesOwned;
+    // List of territories owned by the player
+    std::vector<Territory*> territoriesOwned;
 
-        // List of orders owned by the player
-        OrdersList* orders;
+    // List of orders owned by the player
+    OrdersList* orders;
 
-        // Hand of cards owned by the player
-        Hand* hand;
+    // Hand of cards owned by the player
+    Hand* hand;
 
-        string playerName;
+    //NEW: Each player will have a unique ID 
+    static int nextID;
+    int playerID;
+
+    // Player name
+    string name;
+
+    //NEW: Each player will have a reinforcement pool
+    int reinforcementPool;
+
+    //NEW: each player will have a vector of integers for players which they have an alliance with
+    std::vector<int> myAlliesForTheTurn;
+
 
     // Player public methods
-    public:
+public:
 
-        // Constructor
-        Player();
+    // Constructor
+    Player();
 
-        // Copy constructor
-        Player(const Player& other);
+    // Copy constructor
+    Player(const Player& other);
 
-        // Class assignment operator
-        Player& operator=(const Player& other);
+    // Class assignment operator
+    Player& operator=(const Player& other);
 
-        // Stream insertion operator
-        friend std::ostream& operator<<(std::ostream& os, const Player& player);
+    // Stream insertion operator
+    friend std::ostream& operator<<(std::ostream& os, const Player& player);
 
-        // Returns a list of territories that are to be defended
-        std::vector<Territory*> toDefend();
-        
-        // Returns a list of territories that are to be attacked
-        std::vector<Territory*> toAttack();
+    // Returns a list of territories that are to be defended
+    std::vector<Territory*> toDefend();
 
-        // Creates order object and adds it to the list of orders
-        void issueOrder(OrderType type);
+    // Returns a list of territories that are to be attacked
+    std::vector<Territory*> toAttack();
 
-        // Add territory
-        void addTerritory(Territory* territory);
+    // Creates order object and adds it to the list of orders
+    void issueOrder(OrderType type);
 
-        Player(const string& name);
+    // Add territory
+    void addTerritory(Territory* territory);
 
-        // Getter for player name
-        string getName() const;
+    // Destructor to clean up resources
+    ~Player();
 
-        // Setter for player name
-        void setName(const string& name);
+    Player(const std::string& playerName);
 
-        // Destructor to clean up resources
-        ~Player();
+    //NEW methods for a2-part4
+    int getPlayerID();
+    int getReinforcementPool();
+    void setReinforcementPool(int reinforcement);
+    bool isAnyAdjacent(Territory* territory);
+    void addCardToHand(Card* c);
+    void addAlly(int allyID);
+    void clearAllies();
+    bool isAllyPresent(int allyID);
 };
+
 
 
 #endif
