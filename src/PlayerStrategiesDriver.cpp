b4702f37af45fe157490a139e6724d57e1db6d99
Bayazeed Rahman <99050468+blazefire16@users.noreply.github.com>
2023-12-04 12:32:37 -0500

    added aggressivePlayer and neutralPlayer

--- src/PlayerStrategiesDriver.cpp
+++ src/PlayerStrategiesDriver.cpp
@@ -220,6 +220,171 @@ void testPlayerStrategies() {
     //--------------------------------------------------------------------------------------------------
     // End of Cheater Player Strategy
     //--------------------------------------------------------------------------------------------------
+    //--------------------------------------------------------------------------------------------------
+    // Aggressive Player Strategy
+    //--------------------------------------------------------------------------------------------------
+    cout << "-----------Start of Aggressive player -------------\n";
+    Player* aggressivePlayer = new Player();
+    PlayerStrategy* aggressiveStrategy = new AggressivePlayerStrategy(aggressivePlayer);
+    aggressivePlayer->setStrategy(aggressiveStrategy);
+    Player* neutralPlayer3 = new Player();
+    PlayerStrategy* neutralStrategy3 = new NeutralPlayerStrategy(neutralPlayer3);
+    neutralPlayer3->setName("NeutralDummy3");
+    neutralPlayer3->setStrategy(neutralStrategy3);
+    vector<Player*> playerList4;
+    playerList4.push_back(neutralPlayer3);
+    aggressivePlayer->setReinforcementPool(5);
+    
+    // Simulate issuing different types of orders
+    Territory* germany4 = new Territory("Germany", "Europe", aggressivePlayer->getPlayerID());
+    Territory* poland4 = new Territory("Germany", "Europe", aggressivePlayer->getPlayerID());
+    Territory* spain4 = new Territory("Spain", "Europe", neutralPlayer3->getPlayerID());
+    aggressivePlayer->addTerritory(germany4);
+    neutralPlayer3->addTerritory(spain4); 
+    germany4->setOwner(aggressivePlayer);
+    poland4->setOwner(aggressivePlayer);
+    spain4->setOwner(neutralPlayer3);
 
+    //adding units
+    germany4->setNbArmies(5);
+    poland4->setNbArmies(1); 
+    spain4->setNbArmies(14);
+    
+    cout << "Neutral player owns France.\n";
+    //Player 2 owns France
+    Territory* france4 = new Territory("France", "Europe", aggressivePlayer->getPlayerID());
+    aggressivePlayer->addTerritory(france4); 
+    france4->setOwner(aggressivePlayer);
+
+    france4->setNbArmies(10);
+
+    cout << "Germany and France are adjacent to each other.\n";
+    cout << "Spain and France are adjacent to each other.\n"; 
+
+    //Germany is adjacent to France and portugal
+    germany4->AddAdjacentTerritory(france4);
+    france4->AddAdjacentTerritory(germany4);
+
+    //Spain is adjacent to France
+    spain4->AddAdjacentTerritory(france4);
+    france4->AddAdjacentTerritory(spain4);
+
+    // Issue a deploy 
+    cout << "-----------Testing Deploy-------------\n";
+    aggressivePlayer->issueOrder(playerList4, OrderType::Deploy);
+    aggressivePlayer->getOrdersList()->getOrder(0)->execute();
+    cout << "-----------END of Testing Deploy-------------\n\n";
+    // Issue an advance order
+    cout << "-----------Testing Advance-------------\n";
+    aggressivePlayer->issueOrder(playerList4, OrderType::Advance);
+    aggressivePlayer->getOrdersList()->getOrder(1)->execute();
+    cout << "-----------END of Testing Advance-------------\n\n";
+    // Issue a bomb order
+    cout << "-----------Testing Bomb-------------\n";
+    aggressivePlayer->issueOrder(playerList4, OrderType::Bomb);
+    cout << "-----------END of Testing Bomb-------------\n\n";
+     // Issue an airlift order
+    cout << "-----------Testing Airlift-------------\n";
+    aggressivePlayer->issueOrder(playerList4, OrderType::Airlift);
+    aggressivePlayer->getOrdersList()->getOrder(2)->execute();
+    cout << "-----------END of Testing Airlift-------------\n\n"; 
+    // Issue a blockade order
+    cout << "-----------Testing Blockade-------------\n";
+    aggressivePlayer->issueOrder(playerList4, OrderType::Blockade);
+    aggressivePlayer->getOrdersList()->getOrder(3)->execute();
+    cout << "-----------END of Testing Blockade-------------\n\n";
+    // Issue a negotiate order
+    cout << "-----------Testing Negotiate-------------\n";
+    aggressivePlayer->issueOrder(playerList4, OrderType::Negotiate);
+    aggressivePlayer->getOrdersList()->getOrder(4)->execute();
+    cout << "-----------END of Testing Negotiate-------------\n\n"; 
+
+    cout << "-----------End of Aggressive player -------------\n";
+
+    //--------------------------------------------------------------------------------------------------
+    // End of Aggressive Player Strategy
+    //--------------------------------------------------------------------------------------------------
+
+    //--------------------------------------------------------------------------------------------------
+    // Neutral Player Strategy
+    //--------------------------------------------------------------------------------------------------
+    cout << "-----------Start of Neutral player -------------\n";
+    Player* neutralPlayer = new Player();
+    PlayerStrategy* neutralStrategy = new NeutralPlayerStrategy(neutralPlayer);
+    neutralPlayer->setStrategy(neutralStrategy);
+    Player* neutralPlayer2 = new Player();
+    PlayerStrategy* neutralStrategy2 = new NeutralPlayerStrategy(neutralPlayer2);
+    neutralPlayer2->setName("NeutralDummy");
+    neutralPlayer2->setStrategy(neutralStrategy2);
+    vector<Player*> playerList3;
+    playerList3.push_back(neutralPlayer2);
+    neutralPlayer->setReinforcementPool(1000);
+    
+    // Simulate issuing different types of orders
+    Territory* germany3 = new Territory("Germany", "Europe", neutralPlayer->getPlayerID());
+    Territory* spain3 = new Territory("Spain", "Europe", neutralPlayer2->getPlayerID());
+    neutralPlayer->addTerritory(germany3);
+    neutralPlayer2->addTerritory(spain3); 
+    germany3->setOwner(neutralPlayer);
+    spain3->setOwner(neutralPlayer2);
+
+    //adding units
+    germany3->setNbArmies(5);
+    spain3->setNbArmies(3);
+    
+    cout << "Neutral player owns France.\n";
+    //Player 2 owns France
+    Territory* france3 = new Territory("France", "Europe", neutralPlayer->getPlayerID());
+    neutralPlayer->addTerritory(france2); 
+    france3->setOwner(neutralPlayer);
+
+    france3->setNbArmies(10);
+
+    cout << "Germany and France are adjacent to each other.\n";
+    cout << "Spain and France are adjacent to each other.\n"; 
+
+    //Germany is adjacent to France and portugal
+    germany3->AddAdjacentTerritory(france3);
+    france3->AddAdjacentTerritory(germany3);
+
+    //Spain is adjacent to France
+    spain3->AddAdjacentTerritory(france3);
+    france3->AddAdjacentTerritory(spain3);
+
+    // Issue a deploy 
+    cout << "-----------Testing Deploy-------------\n";
+    neutralPlayer->issueOrder(playerList3, OrderType::Deploy);
+    neutralPlayer->getOrdersList()->getOrder(0)->execute();
+    cout << "-----------END of Testing Deploy-------------\n\n";
+    // Issue an advance order
+    cout << "-----------Testing Advance-------------\n";
+    neutralPlayer->issueOrder(playerList3, OrderType::Advance);
+    neutralPlayer->getOrdersList()->getOrder(1)->execute();
+    cout << "-----------END of Testing Advance-------------\n\n";
+    // Issue a bomb order
+    cout << "-----------Testing Bomb-------------\n";
+    neutralPlayer->issueOrder(playerList3, OrderType::Bomb);
+    cout << "-----------END of Testing Bomb-------------\n\n";
+     // Issue an airlift order
+    cout << "-----------Testing Airlift-------------\n";
+    neutralPlayer->issueOrder(playerList3, OrderType::Airlift);
+    neutralPlayer->getOrdersList()->getOrder(2)->execute();
+    cout << "-----------END of Testing Airlift-------------\n\n"; 
+    // Issue a blockade order
+    cout << "-----------Testing Blockade-------------\n";
+    neutralPlayer->issueOrder(playerList3, OrderType::Blockade);
+    neutralPlayer->getOrdersList()->getOrder(3)->execute();
+    cout << "-----------END of Testing Blockade-------------\n\n";
+    // Issue a negotiate order
+    cout << "-----------Testing Negotiate-------------\n";
+    neutralPlayer->issueOrder(playerList3, OrderType::Negotiate);
+    neutralPlayer->getOrdersList()->getOrder(4)->execute();
+    cout << "-----------END of Testing Negotiate-------------\n\n"; 
+
+    cout << "-----------End of Neutral player -------------\n";
+
+    //--------------------------------------------------------------------------------------------------
+    // End of Neutral Player Strategy
+    //--------------------------------------------------------------------------------------------------
     
 }
